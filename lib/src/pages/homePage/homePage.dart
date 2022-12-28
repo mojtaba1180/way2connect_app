77ff5abd34a2cb6df2dc6f9bacd37f327a75a5d4
mohammad asghari <mohammadsgr8@gmail.com>
2022-12-28 12:09:04 -0800

    add home Page

--- lib/src/pages/homePage/homePage.dart
+++ lib/src/pages/homePage/homePage.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:is_first_run/is_first_run.dart';
-import 'package:way2connect/src/components/icon-with-text/icon-with-text.dart';
-import 'package:way2connect/src/constants/app_icon_icons.dart';
-import 'package:way2connect/src/constants/text_strings.dart';
+import 'package:way2connect/src/components/pending-message/pending-message.dart';
+import 'package:way2connect/src/components/tab-bar/tab-bar.dart';
+import 'package:way2connect/src/constants/color.dart';
+import 'package:way2connect/src/constants/sizes.dart';
 import 'package:way2connect/src/pages/auth/login-form.dart';
 
 class HomePage extends StatefulWidget {
@@ -34,12 +35,16 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.black,
+          size: 40,
+        ),
+        titleSpacing: 0,
         elevation: 0,
-        title: const IconWithText(
-          icon: AppIcon.logo,
-          text: tAppName,
-          iconColor: Colors.blue,
-          textColor: Colors.black,
+        title: const Text(
+          "Inbox",
+          style: TextStyle(color: tDark),
         ),
         backgroundColor: Colors.white,
         actions: <Widget>[
@@ -49,37 +54,66 @@ class _HomePageState extends State<HomePage> {
             children: [
               ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.only(right: 0),
+                      padding: const EdgeInsets.only(right: 30),
                       backgroundColor: Colors.transparent,
                       shadowColor: Colors.transparent),
                   onPressed: () async => {await IsFirstRun.reset()},
                   child: const Text(
-                    "reset",
-                    style: TextStyle(color: Colors.black),
+                    "mywebsite.com",
+                    style: TextStyle(
+                        color: Colors.black, fontSize: tPrimaryFontSize),
                   ))
             ],
           )
         ],
       ),
       body: Stack(
-        alignment: Alignment.bottomCenter,
         children: [
           // ignore: prefer_const_constructors
           // ignore: unnecessary_const
-
-          const SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 20, left: 20),
-            child: ElevatedButton(
-                onPressed: () => {
-                      Get.to(() => const LoginForm(),
-                          duration: const Duration(milliseconds: 300),
-                          transition: Transition.fade)
-                    },
-                child: const Text("Login-Page")),
-          )
+          PendingMessage(context),
+          Container(
+              margin: const EdgeInsets.only(top: 180),
+              width: double.infinity,
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              child: Column(
+                children: [
+                  Container(
+                    height: 40,
+                    decoration: BoxDecoration(
+                        color: tLightGray,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(
+                          Icons.search,
+                          size: 30,
+                          color: tDark,
+                        ),
+                        border: InputBorder.none,
+                        filled: true,
+                        hintText: "Search",
+                        fillColor: Colors.transparent,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 400,
+                    child: StackOver(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20, left: 20),
+                    child: ElevatedButton(
+                        onPressed: () => {
+                              Get.to(() => const LoginForm(),
+                                  duration: const Duration(milliseconds: 300),
+                                  transition: Transition.fade)
+                            },
+                        child: const Text("Login-Page")),
+                  )
+                ],
+              )),
         ],
       ),
     );
