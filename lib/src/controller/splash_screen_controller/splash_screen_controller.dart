6345e8667dd40c675e16f3b7b4c4ae47cfa3feff
mohammad asghari <mohammadsgr8@gmail.com>
2022-12-28 06:06:10 -0800

    splashScreen controller update

--- lib/src/controller/splash_screen_controller/splash_screen_controller.dart
+++ lib/src/controller/splash_screen_controller/splash_screen_controller.dart
@@ -1,1 +1,22 @@
-class SplashScreenController {}
+import 'package:get/get.dart';
+import 'package:is_first_run/is_first_run.dart';
+import 'package:way2connect/src/layouts/screens/intro/intro.dart';
+import 'package:way2connect/src/pages/homePage/homePage.dart';
+
+class SplashScreenController extends GetxController {
+  static SplashScreenController get find => Get.find();
+  // ignore: non_constant_identifier_names
+  Future StartSplash(context) async {
+    await Future.delayed(const Duration(seconds: 5));
+    bool firstRun = await IsFirstRun.isFirstRun();
+    // Get.to(() => const Intro());
+    if (firstRun) {
+      Get.off(() => const Intro(),
+          duration: const Duration(seconds: 1), transition: Transition.fade);
+    } else {
+      Get.off(() => const HomePage(),
+          duration: const Duration(seconds: 1), transition: Transition.fade);
+    }
+    // Navigator.pushNamed(context, "/intro");
+  }
+}
